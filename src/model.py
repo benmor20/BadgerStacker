8984e3af1b662cf8740adfa315de3144ecbcd84b
benmor20 <benmor2020@gmail.com>
2022-06-02 09:40:01 -0400

    Increase in difficulty

--- src/model.py
+++ src/model.py
@@ -4,25 +4,33 @@ import src.utils as utils
 
 
 class Badger(pygame.sprite.Sprite):
-    def __init__(self, game, xpos, image_path='media/badger.png'):
+    def __init__(self, game, xpos, badger_num, image_path='media/badger.png'):
         super().__init__()
         self.image = pygame.image.load(utils.path_to(image_path)).convert_alpha()
-        self.rect = self.image.get_rect(center=(xpos, constants.BADGER_SPAWN_HEIGHT))
+        if badger_num == 1:
+            self.rect = self.image.get_rect(center=(xpos, constants.IDEAL_BADGER_HEIGHT))
+        else:
+            self.rect = self.image.get_rect(center=(xpos, constants.BADGER_SPAWN_HEIGHT))
         self._game = game
-        self._direction = 1
+        self._speed_scale = badger_num
         self._moving = True
+        self.badger_num = badger_num
+        print(f'Badger num is {badger_num}')
+        print(f'Speed scale is {self._speed_scale}')
 
     @property
     def moving(self):
         return self._moving
 
     def update(self):
+        super().update()
+        print(f'{self._speed_scale}, {self.moving}')
         if self.moving:
-            self.rect = self.rect.move(0, constants.BADGER_SPEED * self._direction)
-            if self._direction == 1 and self.rect.bottom > constants.MAX_BADGER_POS:
-                self._direction = -1
-            elif self._direction == -1 and self.rect.top < constants.MIN_BADGER_POS:
-                self._direction = 1
+            self.rect = self.rect.move(0, constants.BADGER_SPEED_SCALE * self._speed_scale)
+            if self._speed_scale > 0 and self.rect.bottom > constants.MAX_BADGER_POS:
+                self._speed_scale *= -1
+            elif self._speed_scale < 0 and self.rect.top < constants.MIN_BADGER_POS:
+                self._speed_scale *= -1
         self.rect = self.rect.move(-constants.SCROLL_SPEED, 0)
 
     def stop(self):
@@ -69,7 +77,7 @@ class Game:
             if isinstance(s, Player):
                 continue
             s.kill()
-        self.last_badger = Badger(self, constants.SCREEN_WIDTH - 150)
+        self.last_badger = Badger(self, constants.SCREEN_WIDTH - 150, 1)
         self.last_badger.stop()
         self.badgers.add(self.last_badger)
         self.all_sprites.add(self.last_badger)
@@ -79,7 +87,7 @@ class Game:
 
     def create_badger(self):
         new_xpos = self.last_badger.rect.centerx + self.last_badger.rect.width
-        badger = Badger(self, new_xpos)
+        badger = Badger(self, new_xpos, self.last_badger.badger_num + 1)
         self.badgers.add(badger)
         self.all_sprites.add(badger)
         self.last_badger = badger
